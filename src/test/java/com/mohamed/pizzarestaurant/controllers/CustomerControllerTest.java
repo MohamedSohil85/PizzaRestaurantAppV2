0fb36491c0a6c34bc74151e32886c59177f4853c
MohamedSohil85 <mimo_mnb@hotmail.com>
2021-05-04 14:21:29 +0200

    push new files

--- src/test/java/com/mohamed/pizzarestaurant/controllers/CustomerControllerTest.java
+++ src/test/java/com/mohamed/pizzarestaurant/controllers/CustomerControllerTest.java
@@ -5,15 +5,19 @@ import static org.hamcrest.Matchers.is;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mohamed.pizzarestaurant.entities.Customer;
+import com.mohamed.pizzarestaurant.repositories.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,13 +28,15 @@ import org.w3c.dom.stylesheets.LinkStyle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = CustomerController.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CustomerControllerTest {
 
     @Autowired
@@ -58,29 +64,6 @@ public class CustomerControllerTest {
         assertThat(HttpStatus.OK.value(),is(mvcResult.getResponse().getStatus()));
     }
 
-    @Test
-    public void loadCustomers() throws Exception {
-        Customer customer=new Customer();
-        customer.setId(5L);
-        customer.setLastName("Sohil");
-        customer.setFirstName("Mohamed");
-        customer.setPhoneNumber("0148795556");
-        customer.setAddress("Darmstadt");
-        customer.setToken("14sdo4I-4b87-12d3-a456-45795512550");
-        List<Customer> customerList=new ArrayList<>();
-        customerList.add(customer);
-        final String payload="[{id\":5,\"lastName\":\"Sohil\",\"firstName\":\"Mohamed\",\"address\":\"Darmstadt\",\"phoneNumber\":\"0148795556\",\"token\":\"14sdo4I-4b87-12d3-a456-45795512550\"}]";
-
-      //Mockito.when(customerController.loadCustomers()).thenReturn(customerList);
-
-        RequestBuilder requestBuilder=MockMvcRequestBuilders.get("/customers")
-                .contentType(MediaType.APPLICATION_JSON_VALUE);
-        MvcResult result=mockMvc.perform(requestBuilder)
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().is2xxSuccessful()).andReturn();
-
-
-    }
 
     @Test
     public void getCustomerById() throws Exception {
---
+++ src/test/java/com/mohamed/pizzarestaurant/controllers/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package com.mohamed.pizzarestaurant.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductControllerTest {
+
+    @Test
+    public void saveNewProduct() {
+    }
+
+    @Test
+    public void loadAllProducts() {
+    }
+
+    @Test
+    public void getProductById() {
+    }
+}
